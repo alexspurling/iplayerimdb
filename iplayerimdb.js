78677448955db79d8ac6d53068c811cec8afa950
Alex Spurling <alex.spurling@playfish.com>
2013-04-16 21:23:14 +0100

    Added ratings for popular films

--- iplayerimdb.js
+++ iplayerimdb.js
@@ -10,43 +10,60 @@ function cacheRating(title, rating, imdburl) {
 
 function appendRating(rating, imdburl, filmElement, marginTop) {
     var imageurl = chrome.extension.getURL('imdb-small.png')
-    var html = ['<a style="display:inline" target="_blank" href="',imdburl,'"><img style="margin-top:',marginTop,'; margin-right:5px" src="',imageurl,'">',
+    var html = ['<a style="display:inline" target="_blank" href="',imdburl,'"><img style="margin-top:',marginTop,'; margin-right:5px; margin-bottom:0px" src="',imageurl,'">',
                 '<span class="title" style="display:inline; font-size:12px">',rating,'</span><span class="title" style="display:inline; font-size:11px">/10</span></a>'].join('')
     $(filmElement).append(html);
 }
 
-function getAndAppendRating(filmElements, marginTop) {
+function getAndAppendRating(filmElement, title, marginTop) {
+    var cachedRating = getRatingFromCache(title)
+    if (cachedRating) {
+        appendRating(cachedRating.rating, cachedRating.imdburl, filmElement, marginTop);
+    }else{
+        $.getJSON('http://imdbapi.org/', {'q':title}).done(function (data) {
+            var ratingNum = new Number(data[0].rating)
+            var rating = ratingNum.toPrecision(2)
+            var imdburl = data[0].imdb_url
+            cacheRating(title, rating, imdburl)
+            appendRating(rating, imdburl, filmElement, marginTop)
+        });
+    }
+}
+
+function getAndAppendRatingForElements(filmElements, marginTop) {
     filmElements.each(function (index, filmElement) {
         var title = $(filmElement).find('a').attr('title')
         console.log("Loading film data for " + title)
+        getAndAppendRating(filmElement, title, marginTop)
+    });
+}
 
-        var cachedRating = getRatingFromCache(title)
-        if (cachedRating) {
-            appendRating(cachedRating.rating, cachedRating.imdburl, filmElement, marginTop);
-        }else{
-            $.getJSON('http://imdbapi.org/', {'q':title}).done(function (data) {
-                var ratingNum = new Number(data[0].rating)
-                var rating = ratingNum.toPrecision(2)
-                var imdburl = data[0].imdb_url
-                cacheRating(title, rating, imdburl)
-                appendRating(rating, imdburl, filmElement, marginTop)
-            });
-        }
+function getAndAppendRatingForPopularFilms(filmElements, marginTop) {
+    filmElements.each(function (index, filmElement) {
+        var iplayerLink = $(filmElement).find('a').attr('href')
+        //Get the title from the link to the page for this Film as there is no
+        //title attribute set on these links for some reason
+        var title = iplayerLink.replace(/.*\/([^\/]*)\/$/,'$1').replace(/_/g,' ')
+        console.log("Loading film data for popular film: " + title)
+        getAndAppendRating(filmElement, title, marginTop)
     });
 }
 
 function rateFilms() {
     //Film elements with a FILM flag next to them
     var flaggedFilmElements = $('.flag:contains("Film")').parent()
-    getAndAppendRating(flaggedFilmElements, '0px')
+    getAndAppendRatingForElements(flaggedFilmElements, '0px')
     //Films displayed in all categories view
     var allCategoriesFilmElements = $('a.episode-category:contains(Films)').parent().find('h3')
-    getAndAppendRating(allCategoriesFilmElements, '0px')
+    getAndAppendRatingForElements(allCategoriesFilmElements, '0px')
+
+    var mostPopularInFilmsElements = $('h2:contains("Most popular in Films")').parent().find('h3')
+    getAndAppendRatingForPopularFilms(mostPopularInFilmsElements, '0px')
 };
 
 function rateFilmCategory(categoryBody) {
     var categoryFilmElements = categoryBody.find('h3')
-    getAndAppendRating(categoryFilmElements, '0px')
+    getAndAppendRatingForElements(categoryFilmElements, '0px')
 };
 
 function getEpisodeData() {
@@ -120,7 +137,7 @@ $('body').on(
             if (getRatingFromCache(title)) {
                 var filmElement = $(objEvent.target).find('h1')
                 filmElement.find('a').css('display', 'block')
-                getAndAppendRating(filmElement, '4px');
+                getAndAppendRatingForElements(filmElement, '4px');
             }
         }
     }
